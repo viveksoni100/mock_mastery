851b75a3ed674f4829c7fd6e2abc2ea9f56a77b5
viveksoni100 <viveksoni100@gmail.com>
2024-02-13 10:30:48 +0530

    blank projects added

---
+++ mock-mastery/src/main/java/com/mockmastery/MockMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.mockmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockMasteryApplication.class, args);
+	}
+
+}
---
+++ mock-mastery/src/test/java/com/mockmastery/MockMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockmastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
